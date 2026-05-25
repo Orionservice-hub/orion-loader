0c2b2f2d69b9f023dedd4eb1d37f6fbbfb62fea9
x2n4t <fayintz@gmail.com>
2026-05-25 20:17:21 +0700

    Update load.js

--- api/load.js
+++ api/load.js
@@ -1,1 +1,11 @@
-# orion-loader
+export default async function handler(req, res) {
+
+    const response = await fetch(
+        "https://raw.githubusercontent.com/Orionservice-hub/.gg-orionservice.ontop/refs/heads/main/Orionservice.loader"
+    );
+
+    const text = await response.text();
+
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(text);
+}
